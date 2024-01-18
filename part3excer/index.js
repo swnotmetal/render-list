faa1ea745d9ca06d6ee9a2987ba38e8727e34f3a
Shuang.W <66452432+swnotmetal@users.noreply.github.com>
2024-01-18 07:47:54 +0200

    front and back

---
+++ part3excer/index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+];
+
+// Endpoint to get individual notes by ID
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find(note => note.id === id);
+  response.json(note);
+});
+
+// Endpoint to get the whole list of notes
+app.get('/api/notes', (request, response) => {
+  response.json(notes);
+});
+
+const generateID =() => {
+
+  const maxId = notes.length > 0
+  ? Math.max(...notes.map(n => n.id))
+  :0  
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+ 
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json ({
+      error: ' content missing!'
+    })
+  }
+  
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false, //default set to false if no value
+    id: generateID()
+  }
+
+  notes = notes.concat(note)
+  console.log(note)  
+  response.json(note)
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
